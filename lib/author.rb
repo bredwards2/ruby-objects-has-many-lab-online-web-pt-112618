e83e82848c17b01c5f228241d1671022168482bd
Britny Edwards <bredwards2@gmail.com>
2019-01-13 04:25:47 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,23 +1,28 @@
 class Author
- attr_accessor :name, :posts
- 
- def initialize(name)
-   @name = name
-   @posts = []
- end
- 
- def add_post(name)
-   @posts << post
-   author.post = self
- end
- 
-   def add_post_by_name(name)
-   post = Author.new(name)
-   @posts << post
-   post.author = self
-  end 
- 
- def posts
-   @posts
- end
+
+  attr_accessor :name, :posts
+
+  @@post_count = 0
+  
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def add_post(post)
+    @posts << post
+    post.author = self
+    @@post_count += 1
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    @posts << post
+    post.author = self
+    @@post_count += 1
+  end
+
+  def self.post_count
+    @@post_count
+  end
 end
