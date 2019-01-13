882a648452adeb2541793cddfa6f0ec370cb31a5
Britny Edwards <bredwards2@gmail.com>
2019-01-13 04:24:48 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,23 +1,31 @@
 class Artist
- attr_accessor :name
- 
- def initialize(name)
-   @name = name
-   @songs = []
- end
- 
- def add_song(song)
-   @songs << song
-   song.artist = self
- end
- 
- def add_song_by_name(name)
-   song = Song.new(name)
-   @songs << song
-   song.artist = self
-  end 
- 
- def songs
-   @songs
- end
+  attr_accessor :name
+
+  @@song_count = 0
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    self.songs << song
+    song.artist = self
+    @@song_count +=1
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    self.songs << song
+    song.artist = self
+    @@song_count +=1
+  end
+
+  def songs
+    @songs 
+  end
+
+  def self.song_count
+    @@song_count
+  end
 end
