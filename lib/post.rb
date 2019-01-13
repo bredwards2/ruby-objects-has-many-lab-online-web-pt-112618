f85735c3b1cbead7ce6addd47a2927196f3c147c
Britny Edwards <bredwards2@gmail.com>
2019-01-13 04:26:06 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,18 +1,15 @@
 class Post
- attr_accessor :author, :title
- 
- def initialize(author, title)
-   @author = author
-   @title = title
-   @posts = []
- end
- 
- def add_post_by_title(title)
-   @posts << title
-   post.title = self
- end
- 
- def post
-   @post
- end
+
+  attr_accessor :title, :author
+  def initialize(title)
+    @title = title
+  end
+
+  def author_name
+    if @author == nil
+      return nil
+    else
+      @author.name
+    end
+  end
 end
